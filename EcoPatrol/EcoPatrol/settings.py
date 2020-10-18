1cae7f8b0166d456d19ad3fbea440680ab4d2ce7
a.kolyvanov <a.kolyvanov@napoleonit.ru>
2020-10-18 12:31:02 +0500

    add middleware

--- EcoPatrol/EcoPatrol/settings.py
+++ EcoPatrol/EcoPatrol/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mainapp',
-    'frontend'
+    'mainapp'
 ]
 
 MIDDLEWARE = [
@@ -49,7 +48,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'EcoPatrol.middleware.dev_cors_middleware'
 ]
+
 
 ROOT_URLCONF = 'EcoPatrol.urls'
 
