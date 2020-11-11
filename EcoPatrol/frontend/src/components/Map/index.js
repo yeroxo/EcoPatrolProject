e17552380c44b80be495bc2e92b80f2fc67aaeee
LordOfTheDeadlines <49224188+LordOfTheDeadlines@users.noreply.github.com>
2020-11-11 14:22:02 +0500

    add method for map placemarks

--- EcoPatrol/frontend/src/components/Map/index.js
+++ EcoPatrol/frontend/src/components/Map/index.js
@@ -4,11 +4,37 @@ import placemarks from '../placemarks.json'
 import { YMaps, Map, ObjectManager} from 'react-yandex-maps';
 
 export default class MyMap extends React.Component{
+    objectManager = React.createRef();
+    map = React.createRef();
+    ymaps = React.createRef();
+
+    getVisibleObjects = () => {
+        let count = 0;
+        if (this.objectManager.current) {
+            this.objectManager.current.objects.each(obj => {
+                const isShown = this.ymaps.current.util.bounds.containsPoint(
+                    this.map.current.getBounds(),
+                    obj.geometry.coordinates
+                );
+                if (isShown) {
+                    count++;
+                }
+            });
+        }
+        console.log(count);
+        alert(count);
+    };
     render(){
         return (
             <div className='map'>
             <YMaps>
                 <Map
+                    instanceRef={this.map}
+                    onBoundsChange={this.getVisibleObjects}
+                    onLoad={ymapsInstance => {
+                    this.ymaps.current = ymapsInstance;
+                    }}
+                    modules={["util.bounds"]}
                     defaultState={{
                         center: [55.4, 61.7],
                          zoom: 9 
@@ -17,6 +43,7 @@ export default class MyMap extends React.Component{
                     width='90vw'
                 >
                     <ObjectManager
+                        instanceRef={this.objectManager}
                         options={{
                             clusterize: true,
                             gridSize: 32,
