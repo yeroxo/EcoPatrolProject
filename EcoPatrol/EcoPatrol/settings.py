383e566f40c385decc773df29857d4c8bc0918f6
a.kolyvanov <a.kolyvanov@napoleonit.ru>
2020-12-15 18:15:26 +0500

    fix fix ifx

--- EcoPatrol/EcoPatrol/settings.py
+++ EcoPatrol/EcoPatrol/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -89,14 +88,13 @@ WSGI_APPLICATION = 'EcoPatrol.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME':'hello_django_dev',
+        'NAME': 'hello_django_dev',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': '0.0.0.0',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
